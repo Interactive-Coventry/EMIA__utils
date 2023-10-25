445fe3f036b9a7edb8c8cf6c1e84e33a26b38781
foxelas <foxelas@outlook.com>
2023-10-26 01:27:54 +0300

    Update database_utils.py

--- emia_utils/database_utils.py
+++ emia_utils/database_utils.py
@@ -10,6 +10,8 @@ from libs.foxutils.utils.core_utils import settings
 logger = logging.getLogger("emia_utils.database_utils")
 
 READ_DB_CREDENTIALS_FROM = settings["TOKENS"]["read_from"]
+USE_STREAMLIT_DB = bool(eval(settings["DATABASE"]["use_streamlit_db"]))
+
 
 def get_connection_parameters(host=None, port=None, dbname=None, user=None, password=None):
     if READ_DB_CREDENTIALS_FROM == "local":
@@ -25,7 +27,7 @@ def get_connection_parameters(host=None, port=None, dbname=None, user=None, pass
         if password is None:
             password = settings["DATABASE"]["password"]
 
-    elif READ_DB_CREDENTIALS_FROM == "secrets":
+    elif USE_STREAMLIT_DB or READ_DB_CREDENTIALS_FROM == "secrets":
         import streamlit as st
         logger.debug(f"Reading from secrets")
         if host is None:
@@ -50,7 +52,7 @@ def engine_connect():
     conn_string = f"postgresql://[{host}]:{port}/{dbname}?user={user}&password={password}"
     db = create_engine(conn_string)
     conn = db.connect()
-    logger.info(f"Engine connect:Connecting to {conn} from secrets.")
+    logger.debug(f"Engine connect:Connecting to {conn} from secrets.")
 
     return conn
 
@@ -71,10 +73,9 @@ def connect(host=None, port=None, dbname=None, user=None, password=None):
         if READ_DB_CREDENTIALS_FROM == "local":
             conn = connect_with_psycopg2(host, port, dbname, user, password)
 
-        elif READ_DB_CREDENTIALS_FROM == "secrets":
-            #import streamlit as st
-            #conn = st.experimental_connection("postgresql", type="sql")
-            conn = connect_with_psycopg2(host, port, dbname, user, password)
+        elif USE_STREAMLIT_DB:
+            import streamlit as st
+            conn = st.experimental_connection("postgresql", type="sql")
 
         else:
             raise ValueError(f"No connection to database for settings {READ_DB_CREDENTIALS_FROM}.")
@@ -86,13 +87,17 @@ def connect(host=None, port=None, dbname=None, user=None, password=None):
         return None
 
 
-def execute_commands(commands):
+def execute_commands(commands, target_function=None, **kwargs):
+    results = None
     try:
         conn = connect()
-        logger.debug(f"Executing commands: {commands}")
         cur = conn.cursor()
         for command in commands:
+            logger.debug(f"Executing command: {command}")
             cur.execute(command)
+            if target_function is not None:
+                result = target_function(cur, **kwargs)
+                results.append(result)
         cur.close()
         conn.commit()
 
@@ -101,50 +106,51 @@ def execute_commands(commands):
     finally:
         if conn is not None:
             conn.close()
+        return results
 
 
-def execute_command(command):
-    execute_commands([command])
+def execute_command(command, target_function=None, **kwargs):
+    results = execute_commands([command], target_function, **kwargs)
+    return results[0]
 
-
-def execute_command_and_do_something(command, target_function, **kwargs):
-    result = None
-    try:
+def query_with_streamlit(command, conn=None):
+    df = None
+    if conn is None:
         conn = connect()
-        logger.debug(f"Executing command: {command}")
-        cur = conn.cursor()
-        cur.execute(command)
-        result = target_function(cur, **kwargs)
-        cur.close()
-        conn.commit()
-    except (Exception, psycopg2.DatabaseError) as error:
-        logger.error(error)
-    finally:
-        if conn is not None:
-            conn.close()
-        return result
+    df = conn.query(command)
+    return df
 
+def execute_command_with_streamlit(command, conn=None):
+    if conn is None:
+        conn = connect()
+    with conn.session as s:
+        s.execute(command)
+        s.commit()
 
 def fetch_one(cur):
     result = cur.fetchone()
     return result
 
 
-def check_connection():
+def check_connection(conn=None):
     """ Connect to the PostgreSQL database server """
     # connect to the PostgreSQL server
     logger.info('Connecting to the PostgreSQL database...')
 
     # execute a statement
-    logger.info('PostgreSQL database version:')
     command = 'SELECT version()'
-    db_version = execute_command_and_do_something(command, fetch_one)
-    logger.info(db_version)
+    if USE_STREAMLIT_DB:
+        df = query_with_streamlit(command, conn)
+        if df is not None:
+            db_version = df.iloc[0]["version"]
+    else:
+        db_version = execute_command(command, fetch_one)
+    logger.info(f"PostgreSQL database version: {db_version}")
 
 
 def drop_table(table_name):
-    logger.info(f'Dropping table: {table_name}')
-    command = f'drop table if exists {table_name}'
+    logger.info(f"Dropping table: {table_name}")
+    command = f"drop table if exists {table_name}"
     execute_command(command)
 
 def create_vehicle_count_table():
@@ -193,69 +199,98 @@ def create_tables():
     execute_commands(commands)
 
 
-def retrieve_primary_key(table_name):
+def retrieve_primary_key(table_name, conn=None):
     command = f"SELECT a.attname, format_type(a.atttypid, a.atttypmod) AS data_type\nFROM pg_index i\n" \
               f"JOIN pg_attribute a ON a.attrelid = i.indrelid  AND a.attnum = ANY(i.indkey)\n" \
               f"WHERE i.indrelid = '{table_name}'::regclass AND i.indisprimary;"
-    result = execute_command_and_do_something(command, fetch_one)
-    if result is not None:
-        return result[0]
-    return None
+
+    result = None
+    if USE_STREAMLIT_DB:
+        df = query_with_streamlit(command, conn)
+        if df is not None:
+            result = df.iloc[0]["attname"]
+    else:
+        result = execute_command(command, fetch_one)
+        if result is not None:
+            result = result[0]
+    return result
 
 
-def set_primary_key_from_df(df, table_name):
+def set_primary_key_from_df(df, table_name, conn=None):
     current_primary_key = retrieve_primary_key(table_name)
     if current_primary_key is not None and current_primary_key != df.index.name:
-        command = f'ALTER TABLE {table_name} ADD PRIMARY KEY ( {df.index.name});'
-        execute_command(command)
+        command = f"ALTER TABLE {table_name} ADD PRIMARY KEY ( {df.index.name});"
+        if USE_STREAMLIT_DB:
+            execute_command_with_streamlit(command, conn)
+        else:
+            execute_command(command)
 
 
-def replace_df_to_table(df, table_name):
-    df.to_sql(table_name, engine_connect(), if_exists='replace')
-    set_primary_key_from_df(df, table_name)
+def replace_df_to_table(df, table_name, conn=None):
+    if USE_STREAMLIT_DB:
+        df.to_sql(table_name, engine_connect(), if_exists="replace")
+        set_primary_key_from_df(df, table_name, conn)
+
+    else:
+        df.to_sql(table_name, engine_connect(), if_exists="replace")
+        set_primary_key_from_df(df, table_name)
 
 
-def append_df_to_table(df, table_name, append_only_new=True):
+def append_df_to_table(df, table_name, append_only_new=True, conn=None):
     try:
         if append_only_new:
             index = df.index.name
-            db_start_index, db_end_index = get_min_max_primary_key(table_name, index)
+            db_start_index, db_end_index = get_min_max_primary_key(table_name, index, conn)
             if db_start_index is not None and db_end_index is not None:
                 logger.debug(
-                    f'Table [{table_name}] with index [{index}] has start index {db_start_index} and end index {db_end_index}.')
+                    f"Table [{table_name}] with index [{index}] has start index {db_start_index} and end index {db_end_index}.")
                 df = df.loc[(df.index.to_pydatetime() < db_start_index) | (df.index.to_pydatetime() > db_end_index)]
 
         if len(df) > 0:
-            df.to_sql(table_name, engine_connect(), if_exists='append', schema='public', chunksize=50)
-            set_primary_key_from_df(df, table_name)
-            logger.debug(f'Appending values outside current bounds only (Total new values: {len(df)}).')
+            if USE_STREAMLIT_DB:
+                df.to_sql(table_name, engine_connect(), if_exists="append", schema="public", chunksize=50)
+                logger.debug(f"Streamlit connect: appended {len(df)}")
+                set_primary_key_from_df(df, table_name, conn)
+            else:
+                df.to_sql(table_name, engine_connect(), if_exists="append", schema="public", chunksize=50)
+                set_primary_key_from_df(df, table_name)
+            logger.debug(f"Appended values outside current bounds only (Total new values: {len(df)}).")
 
         else:
-            logger.debug('Nothing to append.')
+            logger.debug("Nothing to append.")
 
     except IntegrityError as e:
-        logger.error(f"IntegrityError: {e}")
+        logger.debug(f"IntegrityError: {e}")
         logger.error("Can't append, because key exists")
 
 
-def get_min_max_primary_key(table_name, id_name=None):
+def get_min_max_primary_key(table_name, id_name=None, conn=None):
     if id_name is None:
         id_name = retrieve_primary_key(table_name)
 
-    command = f'SELECT MIN({id_name}), MAX({id_name})  from {table_name};'
-    result = execute_command_and_do_something(command, fetch_one)
-    if result is None:
-        None, None
+    min = None
+    max = None
+    command = f"SELECT MIN({id_name}), MAX({id_name})  from {table_name};"
+    if USE_STREAMLIT_DB:
+        df = query_with_streamlit(command, conn)
+        if df is not None:
+            min = df.iloc[0]["min"]
+            max = df.iloc[0]["max"]
     else:
-        return result[0], result[1]
+        result = execute_command(command, fetch_one)
+        if result:
+            min = result[0]
+            max = result[1]
+
+    return min, max
 
 
 def get_timezone():
-    timezone = 'Asia/Singapore'
-    column = 'datetime'
-    table_name = 'weather'
-    command = f'select {column} AT TIME ZONE {timezone} from {table_name};'
-
+    timezone = "Asia/Singapore"
+    column = "datetime"
+    table_name = "weather"
+    command = f"select {column} AT TIME ZONE {timezone} from {table_name};"
+    execute_command(command)
 
 def enclose_in_quotes(input_str):
     output_str = "'" + str(input_str) + "'"
@@ -263,25 +298,24 @@ def enclose_in_quotes(input_str):
 
 
 def read_table_with_select(table_name, params=None, conn=None):
-    command = f'SELECT * FROM {table_name} '
+    command = f"SELECT * FROM {table_name} "
     if len(params) > 0:
-        where_clause = 'WHERE '
+        where_clause = "WHERE "
         command = command + where_clause
         for vals in params:
             command = command + ' '.join(vals) + ' '
-
-    if conn is None:
-        conn = engine_connect()
-
     logger.debug(f"Reading table with select: {command}")
-    if isinstance(conn, Connection): # For SQLAlchemy
+
+    if USE_STREAMLIT_DB:
+        df = conn.query(command)
+    else:
+        if conn is None:
+            conn = engine_connect()
+        #if isinstance(conn, Connection):  # For SQLAlchemy
         command = text(command)
         df = pd.read_sql(command, conn)
-    if type(conn).__name__ == "SQLConnection": # For streamlit
-        df = conn.query(command)
-
     return df
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     check_connection()
