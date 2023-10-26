04d1cb6a9b5bf1344cc003af525c72444a3f2031
foxelas <foxelas@outlook.com>
2023-10-26 14:50:38 +0300

    Update database_utils.py

--- emia_utils/database_utils.py
+++ emia_utils/database_utils.py
@@ -10,7 +10,49 @@ from libs.foxutils.utils.core_utils import settings
 logger = logging.getLogger("emia_utils.database_utils")
 
 READ_DB_CREDENTIALS_FROM = settings["TOKENS"]["read_from"]
-USE_STREAMLIT_DB = bool(eval(settings["DATABASE"]["use_streamlit_db"]))
+DB_MODE = settings["DATABASE"]["db_mode"]  # "local" or "streamlit" or "firebase"
+USES_STREAMLIT = DB_MODE == "streamlit"
+USES_FIREBASE = DB_MODE == "firebase"
+
+
+def init_firebase():
+    import firebase_admin
+    from firebase_admin import credentials
+    from google.oauth2 import service_account
+    import streamlit as st
+    from google.cloud import firestore
+
+    key_dict = dict(st.secrets["firebase"])
+    FIREBASE_PROJECT_NAME = settings["FIREBASE"]["project_name"]
+    creds = service_account.Credentials.from_service_account_info(key_dict)
+    db = firestore.Client(credentials=creds, project=FIREBASE_PROJECT_NAME)
+    return db
+
+
+def collection_reference_to_dataframe(db_collection, is_list=False):
+    if not is_list:
+        table = list(db_collection.stream())
+    else:
+        table = db_collection
+    table_dict = list(map(lambda x: x.to_dict(), table))
+    df = pd.DataFrame(table_dict)
+    return df
+
+
+def insert_row_to_firebase(db, row_dict, table_name, id_name=None):
+    if id_name is None:
+        update_time, added_ref = db.collection(table_name).add(row_dict)
+        logger.debug(f"Added document with id {added_ref.id} to {table_name} at {update_time}.")
+    else:
+        document_id = row_dict[id_name]
+        doc_ref = db.collection(table_name).document(document_id)
+        doc = doc_ref.get()
+        if doc.exists:
+            logger.debug(f"Document with id {document_id} already exists in {table_name}.")
+        else:
+            db.collection(table_name).document(document_id).set(row_dict)
+            logger.debug(f"Added document with id {document_id} to {table_name}.")
+
 
 
 def get_connection_parameters(host=None, port=None, dbname=None, user=None, password=None):
@@ -27,7 +69,7 @@ def get_connection_parameters(host=None, port=None, dbname=None, user=None, pass
         if password is None:
             password = settings["DATABASE"]["password"]
 
-    elif USE_STREAMLIT_DB or READ_DB_CREDENTIALS_FROM == "secrets":
+    elif USES_STREAMLIT or READ_DB_CREDENTIALS_FROM == "secrets":
         import streamlit as st
         logger.debug(f"Reading from secrets")
         if host is None:
@@ -50,7 +92,7 @@ def get_connection_parameters(host=None, port=None, dbname=None, user=None, pass
 def engine_connect():
     host, port, dbname, user, password = get_connection_parameters()
     conn_string = f"postgresql://[{host}]:{port}/{dbname}?user={user}&password={password}"
-    db = create_engine(conn_string)
+    db = create_engine(conn_string)  # , pool_pre_ping=True
     conn = db.connect()
     logger.debug(f"Engine connect:Connecting to {conn} from secrets.")
 
@@ -68,12 +110,13 @@ def connect_with_psycopg2(host=None, port=None, dbname=None, user=None, password
     logger.info(f"Connecting to {conn} from {READ_DB_CREDENTIALS_FROM}.")
     return conn
 
+
 def connect(host=None, port=None, dbname=None, user=None, password=None):
     try:
         if READ_DB_CREDENTIALS_FROM == "local":
             conn = connect_with_psycopg2(host, port, dbname, user, password)
 
-        elif USE_STREAMLIT_DB:
+        elif USES_STREAMLIT:
             import streamlit as st
             conn = st.experimental_connection("postgresql", type="sql")
 
@@ -113,6 +156,7 @@ def execute_command(command, target_function=None, **kwargs):
     results = execute_commands([command], target_function, **kwargs)
     return results[0]
 
+
 def query_with_streamlit(command, conn=None):
     df = None
     if conn is None:
@@ -120,12 +164,14 @@ def query_with_streamlit(command, conn=None):
     df = conn.query(command)
     return df
 
+
 def execute_command_with_streamlit(command, conn=None):
     if conn is None:
         conn = connect()
     with conn.session as s:
         s.execute(command)
         s.commit()
+
 
 def fetch_one(cur):
     result = cur.fetchone()
@@ -139,7 +185,7 @@ def check_connection(conn=None):
 
     # execute a statement
     command = 'SELECT version()'
-    if USE_STREAMLIT_DB:
+    if USES_STREAMLIT:
         df = query_with_streamlit(command, conn)
         if df is not None:
             db_version = df.iloc[0]["version"]
@@ -152,6 +198,7 @@ def drop_table(table_name):
     logger.info(f"Dropping table: {table_name}")
     command = f"drop table if exists {table_name}"
     execute_command(command)
+
 
 def create_vehicle_count_table():
     commands = (
@@ -172,6 +219,7 @@ def create_vehicle_count_table():
         """,
     )
     execute_commands(commands)
+
 
 def create_tables():
     """ create tables in the PostgreSQL database"""
@@ -205,7 +253,7 @@ def retrieve_primary_key(table_name, conn=None):
               f"WHERE i.indrelid = '{table_name}'::regclass AND i.indisprimary;"
 
     result = None
-    if USE_STREAMLIT_DB:
+    if USES_STREAMLIT:
         df = query_with_streamlit(command, conn)
         if df is not None:
             result = df.iloc[0]["attname"]
@@ -220,14 +268,14 @@ def set_primary_key_from_df(df, table_name, conn=None):
     current_primary_key = retrieve_primary_key(table_name)
     if current_primary_key is not None and current_primary_key != df.index.name:
         command = f"ALTER TABLE {table_name} ADD PRIMARY KEY ( {df.index.name});"
-        if USE_STREAMLIT_DB:
+        if USES_STREAMLIT:
             execute_command_with_streamlit(command, conn)
         else:
             execute_command(command)
 
 
 def replace_df_to_table(df, table_name, conn=None):
-    if USE_STREAMLIT_DB:
+    if USES_STREAMLIT:
         df.to_sql(table_name, engine_connect(), if_exists="replace")
         set_primary_key_from_df(df, table_name, conn)
 
@@ -247,7 +295,7 @@ def append_df_to_table(df, table_name, append_only_new=True, conn=None):
                 df = df.loc[(df.index.to_pydatetime() < db_start_index) | (df.index.to_pydatetime() > db_end_index)]
 
         if len(df) > 0:
-            if USE_STREAMLIT_DB:
+            if USES_STREAMLIT:
                 df.to_sql(table_name, engine_connect(), if_exists="append", schema="public", chunksize=50)
                 logger.debug(f"Streamlit connect: appended {len(df)}")
                 set_primary_key_from_df(df, table_name, conn)
@@ -271,7 +319,7 @@ def get_min_max_primary_key(table_name, id_name=None, conn=None):
     min = None
     max = None
     command = f"SELECT MIN({id_name}), MAX({id_name})  from {table_name};"
-    if USE_STREAMLIT_DB:
+    if USES_STREAMLIT:
         df = query_with_streamlit(command, conn)
         if df is not None:
             min = df.iloc[0]["min"]
@@ -292,6 +340,7 @@ def get_timezone():
     command = f"select {column} AT TIME ZONE {timezone} from {table_name};"
     execute_command(command)
 
+
 def enclose_in_quotes(input_str):
     output_str = "'" + str(input_str) + "'"
     return output_str
@@ -306,12 +355,12 @@ def read_table_with_select(table_name, params=None, conn=None):
             command = command + ' '.join(vals) + ' '
     logger.debug(f"Reading table with select: {command}")
 
-    if USE_STREAMLIT_DB:
+    if USES_STREAMLIT:
         df = conn.query(command)
     else:
         if conn is None:
             conn = engine_connect()
-        #if isinstance(conn, Connection):  # For SQLAlchemy
+        # if isinstance(conn, Connection):  # For SQLAlchemy
         command = text(command)
         df = pd.read_sql(command, conn)
     return df
