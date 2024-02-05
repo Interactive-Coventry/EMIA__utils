b4906488a7baaecffb7680937d807bb29f7e493f
foxelas <foxelas@outlook.com>
2024-02-05 15:51:48 +0200

    Update object_detection.py
    Add custom LoadStreams function

--- emia_utils/object_detection.py
+++ emia_utils/object_detection.py
@@ -27,7 +27,7 @@ import torch.backends.cudnn as cudnn
 import cv2
 
 from libs.yolov7.models.experimental import attempt_load
-from libs.yolov7.utils.datasets import LoadStreams, LoadImages, LoadImages_2, letterbox
+from libs.yolov7.utils.datasets import LoadImages, LoadImages_2, letterbox
 from libs.yolov7.utils.general import check_img_size, check_requirements, check_imshow, \
     non_max_suppression, apply_classifier, scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path
 from libs.yolov7.utils.plots import plot_one_box
@@ -55,6 +55,95 @@ def LoadModel(options, device, half, classify=False):
 
     return model, stride, modelc
 
+RETRIEVE_EVERY_N_FRAMES = 5
+USE_PAFY = False
+CUSTOM_FPS = 30
+
+
+class LoadStreams:  # multiple IP or RTSP cameras
+    def __init__(self, sources='streams.txt', custom_fps=None):
+        self.mode = 'stream'
+        self.terminated = False
+
+        if os.path.isfile(sources):
+            with open(sources, 'r') as f:
+                sources = [x.strip() for x in f.read().strip().splitlines() if len(x.strip())]
+        else:
+            sources = [sources]
+
+        n = len(sources)
+        self.imgs = [None] * n
+        self.sources = [clean_str(x) for x in sources]  # clean source names for later
+        for i, s in enumerate(sources):
+            # Start the thread to read frames from the video stream
+            print(f"{i + 1}/{n}: {s}... ", end="")
+            url = eval(s) if s.isnumeric() else s
+            if "youtube.com/" in str(url) or "youtu.be/" in str(url):  # if source is YouTube video
+                if USE_PAFY:
+                    check_requirements(("pafy", "youtube_dl"))
+                    import pafy
+                    url = pafy.new(url).getbest(preftype="mp4").url
+                else:
+                    yt = YouTube(url)
+                    url = yt.streams.filter(file_extension="mp4", res=720).first().url
+
+            cap = cv2.VideoCapture(url)
+            assert cap.isOpened(), f"Failed to open {s}"
+            w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+            h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+            if custom_fps is not None:
+                self.fps = custom_fps
+            else:
+                self.fps = cap.get(cv2.CAP_PROP_FPS) % 100
+
+            _, self.imgs[i] = cap.read()  # guarantee first frame
+            thread = Thread(target=self.update, args=([i, cap]), daemon=True)
+            print(f" success ({w}x{h} at {self.fps:.2f} FPS).")
+            thread.start()
+
+        print("")  # newline
+
+    def update(self, index, cap):
+        # Read next stream frame in a daemon thread
+        n = 0
+        while cap.isOpened():
+            n += 1
+            # _, self.imgs[index] = cap.read()
+            success = cap.grab()
+            if success and n == RETRIEVE_EVERY_N_FRAMES:  # read every nth frame
+                success, im = cap.retrieve()
+                if success:
+                    self.imgs[index] = im if success else self.imgs[index] * 0
+                    n = 0
+
+            time.sleep(1 / self.fps)  # wait time
+
+            if not success:
+                break
+
+        cap.release()
+        cv2.destroyAllWindows()
+        print(f"Failed to retrieve frame from stream. Terminating...")
+        self.terminated = True
+
+    def __iter__(self):
+        self.count = -1
+        return self
+
+    def __next__(self):
+        self.count += 1
+        img0 = self.imgs.copy()
+        if cv2.waitKey(1) == ord("q"):  # q to quit
+            cv2.destroyAllWindows()
+            raise StopIteration
+
+        img = np.ascontiguousarray(img0).squeeze()
+        return img
+
+    def __len__(self):
+        return 0  # 1E12 frames = 32 streams at 30 FPS for 30 years
+
+
 ################################################
 
 class InParams:
@@ -64,7 +153,7 @@ class InParams:
                 setattr(self, key, value)
 
 
-def load_object_detection_model(save_img=True, save_txt=True, device="cuda"):
+def load_object_detection_model(save_img=True, save_txt=True, device="cuda", yolo_model=YOLO_MODEL):
     classify = False
     if device == "cuda" or device == "gpu":
         device = select_device("0")
@@ -98,7 +187,7 @@ def load_object_detection_model(save_img=True, save_txt=True, device="cuda"):
                         stride=0,
                         names=[],
                         colors=[],
-                        weights=YOLO_MODEL + ".pt"))
+                        weights=yolo_model + ".pt"))
 
     save_dir = Path(increment_path(Path(opt.project) / opt.name, exist_ok=opt.exist_ok))
     (save_dir / "labels" if save_txt else save_dir).mkdir(parents=True, exist_ok=True)
